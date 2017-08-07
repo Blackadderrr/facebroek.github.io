eab920a87aed32046fe2c4d2640936093809a673
Blackadder <angelinodesmet@hotmail.com>
2017-08-07 07:38:45 +0200

    removes easter egg, hides my name, adds source, changes a yt link, alien color green

--- js/facebroek.js
+++ js/facebroek.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
     },
     {
       id: "questionmark",
-      source: "https://www.youtube.com/embed/3HH9IiHMD2M?start=17&autoplay=1&rel=0&amp;showinfo=0",
+      source: "https://www.youtube.com/embed/ITKQbv_4bKE?start=0&autoplay=1&rel=0&amp;showinfo=0",
       action: "click"
     },
     {
@@ -51,8 +51,8 @@ document.addEventListener('DOMContentLoaded', function() {
       action: "click"
     },
     {
-      id: "subtractvertising",
-      source: "https://www.youtube.com/embed/ouE-CcwE8Ls?start=0&autoplay=1&rel=0&amp;showinfo=0",
+      id: "myname",
+      source: "https://www.youtube.com/embed/cW2bqBhP4AA?start=0&autoplay=1&rel=0&amp;showinfo=0",
       action: "click"
     }
   ];
@@ -84,16 +84,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-// Quick and dirty, changes some css in the footer
-  document.getElementById("questionmark").onmouseover = function() {
-    document.getElementById("myname").style.color = "rgb(0, 0, 0)";
-  }
-
-  document.getElementById("questionmark").onmouseout = function() {
-    document.getElementById("myname").style.color = "rgb(162, 162, 162)";
-  }
-
-
   //iterates through the links that change yt video source
   for (let i = 0; i < link_Arr.length; i++) {
     document.getElementById(link_Arr[i].id).addEventListener(link_Arr[i].action, function() {
@@ -121,9 +111,9 @@ document.addEventListener('DOMContentLoaded', function() {
       const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
       const width = 21;
       const height = 21;
-      const fsize = 1.5;
+      const fsize = 2;
       const margin = 0;
-      const bgObjectHTML = "<div style='margin:" + margin + "px; height:" + (height - (2 * margin)) + "px; width:" + (width - (2 * margin)) + "px' class='bgfillobject'><p style='font-size: " + fsize + "em'>♥</p></div>";
+      const bgObjectHTML = "<div style='margin:" + margin + "px; height:" + (height - (2 * margin)) + "px; width:" + (width - (2 * margin)) + "px' class='bgfillobject'><p style='font-size: " + fsize + "em'>☺</p></div>";
       let outputHTML = "";
       for (let i = 0; i < Math.floor(viewportWidth / width); i++) {
         for (let j = 0; j < Math.floor(viewportHeight / height); j++) {
@@ -143,12 +133,12 @@ document.addEventListener('DOMContentLoaded', function() {
       const target = e.target;
       const prettyColorIsABadVariableName = "rgb(255, 185, 179)";
       const rareColor = "rgb(255, 0, 0)";
-      if (Math.floor(Math.random() * 1000000) === 42 && target.style.color != rareColor && target.style.color != prettyColorIsABadVariableName) {
-        target.style.color = rareColor;
-        alert("You found the one in a million heart.\nOne of the best songs in the world is yours.");
-        visiblePointerEvents();
-        const yt = document.getElementById("youtube");
-        yt.src = "https://www.youtube.com/embed/cW2bqBhP4AA?start=0&autoplay=1&rel=0&amp;showinfo=0";
+      if (Math.floor(Math.random() * 1000) === 42 && target.style.color != rareColor && target.style.color != prettyColorIsABadVariableName) {
+        // target.style.color = rareColor;
+        // alert("You found the one in a million heart.\nOne of the best songs in the world is yours.");
+        // visiblePointerEvents();
+        // const yt = document.getElementById("youtube");
+        // yt.src = "https://www.youtube.com/embed/cW2bqBhP4AA?start=0&autoplay=1&rel=0&amp;showinfo=0";
       } else if (target.style.color != prettyColorIsABadVariableName && target.style.color != rareColor) {
         target.style.color = prettyColorIsABadVariableName;
       } else if (target.style.color != rareColor) {
