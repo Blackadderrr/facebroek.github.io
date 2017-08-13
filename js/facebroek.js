0c1f24781f25995666b572cc0e4a4eb321127914
Blackadder <angelinodesmet@hotmail.com>
2017-08-13 17:07:36 +0200

    changes song

--- js/facebroek.js
+++ js/facebroek.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
     },
     {
       id: "musicalnote",
-      source: "https://www.youtube.com/embed/oXUj4okEAZg?start=0&autoplay=1&rel=0&amp;showinfo=0",
+      source: "https://www.youtube.com/embed/uzgBD2wysuI?start=0&autoplay=1&rel=0&amp;showinfo=0",
       action: "click"
     },
     {
