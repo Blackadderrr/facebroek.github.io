81b1325f01280ab27ba36ece16a709b6945f9135
Blackadder <angelinodesmet@hotmail.com>
2018-02-09 12:11:59 +0100

    removes smiley bg, adds coloring backg, removes top arrow indicator images

--- js/facebroek.js
+++ js/facebroek.js
@@ -5,8 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     return Math.floor((Math.random() * max) + min);
   }
 
-  const randColor = function(minr,maxr,ming,maxg,minb,maxb){
+  const randColor = function(minr, maxr, ming, maxg, minb, maxb) {
     return "rgb(" + randNumber(minr, maxr) + "," + randNumber(ming, maxg) + "," + randNumber(minb, maxb) + ")";
+  }
+
+  const cycleThreeColors = function(number, max) {
+    const clrNr = number*40;
+    const maxNr = max;
+    if (clrNr) {
+      // console.log(Math.sin(number));
+      return "rgb(" + Math.floor(Math.sin(clrNr/maxNr+10)*180) + "," + Math.floor(Math.sin(clrNr/maxNr+1)*255) + "," + Math.floor(Math.sin(clrNr/maxNr+2)*255) + ")";
+    }
   }
 
   const drawIntroText = () => {
@@ -118,21 +127,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const notification = document.getElementById("triggersaliva");
   if (notification) {
     notification.addEventListener("click", () => {
-      document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60,80,60,150,60,150);
-      setTimeout(function () {
-        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60,100,60,120,60,150);
+      document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60, 80, 60, 150, 60, 150);
+      setTimeout(function() {
+        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60, 100, 60, 120, 60, 150);
       }, 50);
-      setTimeout(function () {
-        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60,120,60,150,60,150);
+      setTimeout(function() {
+        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60, 120, 60, 150, 60, 150);
       }, 100);
-      setTimeout(function () {
-        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60,140,60,150,60,150);
+      setTimeout(function() {
+        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60, 140, 60, 150, 60, 150);
       }, 150);
-      setTimeout(function () {
-        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60,160,60,150,60,150);
+      setTimeout(function() {
+        document.getElementsByClassName("salivate")[0].style.backgroundColor = randColor(60, 160, 60, 150, 60, 150);
       }, 200);
-      setTimeout(function () {
-        document.getElementsByClassName("salivate")[0].style.backgroundColor = "rgba(215, 90, 90, 1)";
+      setTimeout(function() {
+        document.getElementsByClassName("salivate")[0].style.backgroundColor = "rgba(255, 0, 0, 0.51)";
       }, 250);
     });
 
@@ -142,39 +151,85 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   if (!isMobile.any()) {
-    let smileynumber = 0;
-    let happynumber = 0;
-    let sadnumber = 0;
-    let drawBackgroundPattern = () => {
+    // Copied the core functionality from: https://stackoverflow.com/questions/7790725/javascript-track-mouse-position; http://output.jsbin.com/gejuz/1
+    // The color part is mine, that's why it's shitty code.
+
+    document.onmousemove = handleMouseMove;
+    var colorCount = 255;
+    var decrementor = -1;
+
+    function handleMouseMove(event) {
+      var dot, eventDoc, doc, body, pageX, pageY;
       const viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
       const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      const dimension = 32;
-      const bgObjectHTML = '<img class="bgfillobjects" src="./img/sad.gif" alt="sad face">';
+      event = event || window.event; // IE-ism
 
-      let outputHTML = "";
-      for (let i = 0; i < Math.floor(viewportWidth / dimension); i++) {
-        for (let j = 0; j < Math.floor(viewportHeight / dimension); j++) {
-          outputHTML += bgObjectHTML;
-          smileynumber += 1;
-        }
+      // If pageX/Y aren't available and clientX/Y
+      // are, calculate pageX/Y - logic taken from jQuery
+      // Calculate pageX/Y if missing and clientX/Y available
+      if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+          (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+          (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+          (doc && doc.scrollTop || body && body.scrollTop || 0) -
+          (doc && doc.clientTop || body && body.clientTop || 0);
+
       }
-      document.getElementsByClassName("backgroundfill")[0].innerHTML = outputHTML;
+      //start: not from original source
+      if (colorCount == 255) {
+        decrementor = -1;
+      } else if (colorCount == 0) {
+        decrementor = 1;
+      }
+      colorCount += decrementor;
+      //end
+      // Add a dot to follow the cursor
+      dot = document.createElement('div');
+      dot.className = "dot";
+      dot.style.left = event.pageX - 60 + "px";
+      dot.style.top = event.pageY - 60 + "px";
+      dot.style.backgroundColor = cycleThreeColors(colorCount,255); //not from original source
+      document.body.appendChild(dot);
     }
 
-    drawBackgroundPattern();
-
-    window.onresize = () => {
-      drawBackgroundPattern();
-    }
-
-    document.getElementsByClassName("backgroundfill")[0].addEventListener("mouseover", function(e) {
-      const target = e.target;
-      if (target.getAttribute("src") === "./img/sad.gif") {
-        target.src = "./img/happy.gif";
-
-      } else if (target.getAttribute("src") === "./img/happy.gif") {
-        target.src = "./img/sad.gif";
-      }
-    });
+    //Smiley background
+    // let smileynumber = 0;
+    // let happynumber = 0;
+    // let sadnumber = 0;
+    // let drawBackgroundPattern = () => {
+    //   const viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    //   const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    //   const dimension = 32;
+    //   const bgObjectHTML = '<img class="bgfillobjects" src="./img/sad.gif" alt="sad face">';
+    //
+    //   let outputHTML = "";
+    //   for (let i = 0; i < Math.floor(viewportWidth / dimension); i++) {
+    //     for (let j = 0; j < Math.floor(viewportHeight / dimension); j++) {
+    //       outputHTML += bgObjectHTML;
+    //       smileynumber += 1;
+    //     }
+    //   }
+    //   document.getElementsByClassName("backgroundfill")[0].innerHTML = outputHTML;
+    // }
+    // // drawBackgroundPattern();
+    //
+    // window.onresize = () => {
+    //   // drawBackgroundPattern();
+    // }
+    //
+    // document.getElementsByClassName("backgroundfill")[0].addEventListener("mouseover", function(e) {
+    //   const target = e.target;
+    //   if (target.getAttribute("src") === "./img/sad.gif") {
+    //     target.src = "./img/happy.gif";
+    //
+    //   } else if (target.getAttribute("src") === "./img/happy.gif") {
+    //     target.src = "./img/sad.gif";
+    //   }
+    // });
   }
 });
